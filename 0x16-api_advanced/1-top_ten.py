10886848874f576af64cd69cee2ee553fd875a9b
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-09 21:30:37 +0100

    fix: Made file executqble

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 '''queries the Reddit API
-and prints the titles of the first 10 hot posts listed for 
+and prints the titles of the first 10 hot posts listed for
 a given subreddit
 '''
-
 import requests
 
 
 def top_ten(subreddit):
+    """ Queries to Reddit API """
     limit = 10
     url = "https://www.reddit.com/r/{}/top.json?limit={}".format(subreddit,
                                                                  limit)
@@ -18,9 +17,6 @@ def top_ten(subreddit):
         print('None')
         return
     res = res.json()
-    if type(res) is not dict:
-        print('None')
-        return
     res = res.get('data', 0).get('children', 0)
     if res:
         [print(res_t.get('data').get('title')) for res_t in res]
