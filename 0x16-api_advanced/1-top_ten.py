27f3a58424c0b94e95f2f7d453aa2ba803addc3f
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-06-23 19:53:00 +0100

    Revert "Defer"
    This reverts commit 198b087b59ee477ef93bd35ca7760b0b1d6c915e.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''queries the Reddit API
+and prints the titles of the first 10 hot posts listed for
+a given subreddit
+'''
+import requests
+
+
+def top_ten(subreddit):
+    """ Queries to Reddit API """
+    limit = {'limit': 10}
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},
+                       allow_redirects=False, params=limit)
+    if res.status_code != 200:
+        print('None')
+        return
+    res = res.json()
+    res = res.get('data', 0).get('children', 0)
+    if res:
+        [print(res_t.get('data').get('title')) for res_t in res]
+    else:
+        print('None')
